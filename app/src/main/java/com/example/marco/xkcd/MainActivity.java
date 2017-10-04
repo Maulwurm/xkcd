a0eff1fac0d4bd3384deb6d1677386231cbb9e00
Maulwurm <32511910+Maulwurm@users.noreply.github.com>
2017-10-04 23:59:52 +0200

    implement previous latest next methods

--- app/src/main/java/com/example/marco/xkcd/MainActivity.java
+++ app/src/main/java/com/example/marco/xkcd/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.marco.xkcd;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -26,6 +27,25 @@ import okhttp3.ResponseBody;
 public class MainActivity extends AppCompatActivity implements Callback {
 
     private ImageView image;
+    private Comic currentComic;
+
+    public Comic getCurrentComic() {
+        return currentComic;
+    }
+
+    public void setCurrentComic(Comic currentComic) {
+        this.currentComic = currentComic;
+    }
+
+    public int getNextComicId(){
+        int currentId = getCurrentComic().getNum();
+        int nextId = currentId + 1;
+        return nextId;
+    }
+
+    public int getPreviousId(){
+        return getCurrentComic().getNum()-1;
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -33,14 +53,14 @@ public class MainActivity extends AppCompatActivity implements Callback {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-
+                case R.id.navigation_previous:
+                    previous();
                     return true;
-                case R.id.navigation_dashboard:
-
+                case R.id.navigation_latest:
+                    latest();
                     return true;
-                case R.id.navigation_notifications:
-
+                case R.id.navigation_next:
+                    next();
                     return true;
             }
             return false;
@@ -58,7 +78,11 @@ public class MainActivity extends AppCompatActivity implements Callback {
 
         image = (ImageView) findViewById(R.id.image);
 
-        String url = "http://xkcd.com/info.0.json";
+        latest();
+
+    }
+
+    public void performRequest(String url){
 
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
@@ -85,28 +109,43 @@ public class MainActivity extends AppCompatActivity implements Callback {
             String responseString = responseBody.string();
             Logger.json(responseString);
 
-            Gson gson = new Gson();
+            // TODO: 04.10.2017 comic can be null
 
-            final Comic comic = gson.fromJson(responseString,Comic.class);
 
-            // TODO: 04.10.2017 titel und url ausgeben
+            final Comic comic = convertComic(responseString);
 
-            String titel = comic.getTitle();
-            String url = comic.getImg();
-            Logger.d("Titel: %s Url: %s",titel,url);
+            if(comic != null){
+                // TODO: 04.10.2017 titel und url ausgeben
 
-            //change context to main ui
-            Handler mainHandler = new Handler(getMainLooper());
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    showComic(comic);
-                }
-            };
-            mainHandler.post(runnable);
+                String titel = comic.getTitle();
+                String url = comic.getImg();
+                Logger.d("Titel: %s Url: %s",titel,url);
+
+                //change context to main ui
+                Handler mainHandler = new Handler(getMainLooper());
+                Runnable runnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        showComic(comic);
+                    }
+                };
+                mainHandler.post(runnable);
+            } else {
+                Logger.e("Comic = NULL!!!");
+            }
         }
     }
 
+    @Nullable
+    public Comic convertComic(String responseString){
+        Gson gson = new Gson();
+        try {
+            return gson.fromJson(responseString,Comic.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     /**
      * Comic bild laden
@@ -116,5 +155,35 @@ public class MainActivity extends AppCompatActivity implements Callback {
         //Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(imageView);
 
         Picasso.with(this).load(comic.getImg()).into(image);
+
+        // TODO: 04.10.2017  save comic
+        setCurrentComic(comic);
+
+    }
+
+    /**
+     * Navigation
+     */
+
+    public void latest(){
+        String latestUrl = "http://xkcd.com/info.0.json";
+        performRequest(latestUrl);
+    }
+
+    public void next(){
+        int nextId = getNextComicId();
+        String nextUrl = getSpecificUrl(nextId);
+        performRequest(nextUrl);
+    }
+
+    public void previous(){
+        int previousId = getPreviousId();
+        String previousUrl = getSpecificUrl(previousId);
+        performRequest(previousUrl);
+    }
+
+    public String getSpecificUrl(int id){
+        String format = "http://xkcd.com/%1$s/info.0.json";
+        return String.format(format, id);
     }
 }
